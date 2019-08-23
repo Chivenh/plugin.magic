84ecef236487ce2492f82f6e8a8d356b7ed29b0f
Cargo-FH <magic@126.com>
2019-08-23 11:53:27 +0800

    update textDialog

--- src/main/java/com/fhtiger/plugins/magic/toolwindow/WindowTextDialog.java
+++ src/main/java/com/fhtiger/plugins/magic/toolwindow/WindowTextDialog.java
@@ -1,9 +1,26 @@
 package com.fhtiger.plugins.magic.toolwindow;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ui.JBColor;
+import com.intellij.ui.awt.RelativePoint;
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WindowTextDialog extends JDialog {
 	private JPanel contentPane;
@@ -11,42 +28,130 @@ public class WindowTextDialog extends JDialog {
 	private JButton buttonCancel;
 	private JPanel textAreaPanel;
 	private JTextArea textArea;
-	private String content;
+	private JTextArea resultTextArea;
+	private JButton SqlInParams;
+	private JButton Html2Text;
+	private final static String EMPTY="";
 
 	public WindowTextDialog(ToolWindow toolWindow) {
-		setContentPane(contentPane);
+
+		setContentPane(this.contentPane);
 		setModal(true);
 		getRootPane().setDefaultButton(buttonOK);
 
-		buttonOK.addActionListener(e -> onOK());
+		this.buttonOK.addActionListener(e -> onOK());
 
-		buttonCancel.addActionListener(e -> onCancel());
+		this.buttonCancel.addActionListener(e -> {
+			toolWindow.hide(null);
+			onCancel();
+		});
 
 		// call onCancel() when cross is clicked
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-		addWindowListener(new WindowAdapter() {
+		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				onCancel();
 			}
 		});
 
 		// call onCancel() on ESCAPE
-		contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+		this.contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+		this.SqlInParams.addActionListener(this::execSqlInParamsAction);
+		this.Html2Text.addActionListener(this::execHtml2Text);
+
+		/*在点击此按钮时执行复制操作*/
+		this.buttonOK.addActionListener(e->{
+			this.resultTextArea.requestFocus();
+			this.resultTextArea.selectAll();
+			this.resultTextArea.copy();
+			actionDown(this.buttonOK,"Copy Successful!");
+		});
+
+		this.buttonOK.addMouseListener(getMouseEnterAdapter(this.buttonOK));
+		this.buttonCancel.addMouseListener(getMouseEnterAdapter(this.buttonCancel));
+		this.SqlInParams.addMouseListener(getMouseEnterAdapter(SqlInParams));
+		this.Html2Text.addMouseListener(getMouseEnterAdapter(this.Html2Text));
+
+		//自动滚动到末尾
+		this.resultTextArea.setAutoscrolls(true);
+		this.textArea.setAutoscrolls(true);
+
+		this.textArea.setBorder(BorderFactory.createBevelBorder(1));
+		this.resultTextArea.setBorder(BorderFactory.createBevelBorder(1));
+	}
+
+
+	private void execSqlInParamsAction(ActionEvent action){
+		String text = this.textArea.getText();
+
+		if(StringUtils.isNotEmpty(text)){
+			String replace = "(\\r|\\n|\\r\\n)";
+
+			String result = text.replaceAll(replace,"','").replaceFirst(",'$","");
+
+			if(!result.startsWith("'")){
+				result = '\''+result;
+			}
+
+			this.resultTextArea.setText(result);
+		}
+		actionDown(this.SqlInParams,"Transfer Successful!");
+	}
+
+	/**
+	 * @param action 转换html内容为纯文本内容
+	 */
+	private void execHtml2Text(ActionEvent action){
+		String text = this.textArea.getText();
+
+		Document doc = Jsoup.parse(text);
+		String htmlContent ="";
+		if(doc!=null){
+			Element body = doc.body();
+
+			List<String> contents = getNodesText(body);
+
+			htmlContent = String.join("",contents);
+		}
+
+		this.resultTextArea.setText(htmlContent);
+	}
+
+	/**
+	 * @param node 元素节点
+	 * @return 元素文本集
+	 */
+	private List<String> getNodesText(Element node){
+		List<String> texts = new ArrayList<>(0);
+		Elements children = node.children();
+
+		if(children.isEmpty()){
+			texts.add(node.text());
+			if(node.isBlock()){
+				texts.add("\n");
+			}
+			return texts;
+		}
+
+		for (Element child : children) {
+			texts.addAll( getNodesText(child));
+		}
+		if(node.isBlock()){
+			texts.add("\n");
+		}
+		return texts;
 	}
 
 	private void onOK() {
 		// add your code here
-		this.content = this.textArea.getText();
 		dispose();
 	}
 
 	private void onCancel() {
 		// add your code here if necessary
 		dispose();
-	}
-
-	public String getContent() {
-		return content;
+		this.reset();
 	}
 
 	@Override
@@ -54,11 +159,73 @@ public class WindowTextDialog extends JDialog {
 		return contentPane;
 	}
 
-	/*
-	public static void main(String[] args) {
-		WindowTextDialog dialog = new WindowTextDialog();
-		dialog.pack();
-		dialog.setVisible(true);
-		System.exit(0);
-	}*/
+
+	/**
+	 * @param button 按钮
+	 * @param result 提示信息
+	 */
+	private static void actionDown(JButton button,String result) {
+		ApplicationManager.getApplication().invokeLater(() -> {
+			JBPopupFactory factory = JBPopupFactory.getInstance();
+			factory.createHtmlTextBalloonBuilder(result, null, new JBColor(new Color(186, 238, 186), new Color(73, 117, 73)), null)
+					.setFadeoutTime(3000)
+					.createBalloon()
+					.show(RelativePoint.fromScreen(button.getLocationOnScreen()), Balloon.Position.below);
+		});
+	}
+
+	/**
+	 * @param button 按钮
+	 * @return 鼠标事件
+	 */
+	@NotNull private MouseAdapter getMouseEnterAdapter(JButton button) {
+		return new MouseAdapter() {
+			@Override public void mouseEntered(MouseEvent e) {
+				button.setBackground(button.getBackground().darker());
+				button.updateUI();
+//				super.mouseEntered(e);
+			}
+
+			@Override public void mouseMoved(MouseEvent e) {
+				button.setBackground(button.getBackground().brighter());
+				button.updateUI();
+//				super.mouseMoved(e);
+			}
+
+			@Override public void mouseExited(MouseEvent e) {
+				button.setBackground(button.getBackground().brighter());
+				button.updateUI();
+			}
+		};
+	}
+
+	/**
+	 * @param button 按钮
+	 * @return 聚焦事件
+	 */
+	@NotNull
+	private FocusAdapter getFocusAdapter(JButton button) {
+		return new FocusAdapter() {
+			@Override public void focusGained(FocusEvent e) {
+				button.setBackground(button.getBackground().darker());
+				button.updateUI();
+//				super.focusGained(e);
+			}
+
+			@Override public void focusLost(FocusEvent e) {
+				button.setBackground(button.getBackground().brighter());
+				button.updateUI();
+//				super.focusLost(e);
+			}
+		};
+	}
+
+	/**
+	 * 重置输入/输出文本框
+	 */
+	private void reset(){
+		this.textArea.setText(EMPTY);
+		this.resultTextArea.setText(EMPTY);
+	}
+
 }
---
+++ src/main/java/com/fhtiger/plugins/magic/utils/NumTransferUtil.java
@@ -0,0 +1,86 @@
+package com.fhtiger.plugins.magic.utils;
+
+
+/**
+ * 数字转换成英文工具
+ * 
+ * @author LFH
+ * @version 1.0.0
+ * @since  2018年3月6日
+ */
+public final class NumTransferUtil {
+	private static final String[] EN_NUM = { // 基本数词表
+			"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
+			"thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "", "", "", "",
+			"", "", "", "", "", "thirty", "", "", "", "", "", "", "", "", "", "forty", "", "", "", "", "", "", "", "",
+			"", "fifty", "", "", "", "", "", "", "", "", "", "sixty", "", "", "", "", "", "", "", "", "", "seventy", "",
+			"", "", "", "", "", "", "", "", "eighty", "", "", "", "", "", "", "", "", "", "ninety" };
+	private static final String[] EN_UNIT = { "hundred", "thousand", "million",
+			"billion", "trillion", "quintillion" }; // 单位表
+
+	public static String transferCapital(long num) {
+		return transfer(num).toUpperCase();
+	}
+
+	public static String transferCapital(String num) {
+		return transfer(num).toUpperCase();
+	}
+
+	public static String transfer(long num) {
+		// long型参数，
+		return transfer(String.valueOf(num));
+		// 因为long型有极限，所以以字符串参数方法为主
+	}
+
+	public static String transfer(String num) {
+		// 数字字符串参数
+		// 判断字符串是否为数字
+		if (!num.matches("\\d+")) {
+			return String.format("%s is not number", num);
+		}
+		num = num.replaceAll("^[0]*([1-9]*)", "$1");
+		// 把字符串前面的0去掉
+		if (num.length() == 0) {
+			// 如果长度为0，则原串都是0
+			return EN_NUM[0];
+		} else if (num.length() > 9) {
+			// 如果大于9，即大于999999999，限制条件
+			return "too big!";
+		}
+		// 按3位分割分组
+		int count = (num.length() % 3 == 0) ? num.length() / 3 : num.length() / 3 + 1;
+		/*if (count > EN_UNIT.length) {
+			return "too big!";
+		} // 判断组单位是否超过，*/
+			// 可以根据需求适当追加enUnit
+		String[] group = new String[count];
+		for (int i = num.length(), j = group.length - 1; i > 0; i -= 3) {
+			group[j--] = num.substring(Math.max(i - 3, 0), i);
+		}
+		StringBuilder buf = new StringBuilder(); // 结果保存
+		for (int i = 0; i < count; i++) { // 遍历分割的组
+			int v = Integer.valueOf(group[i]);
+			if (v >= 100) { // 因为按3位分割，所以这里不会有超过999的数
+				buf.append(EN_NUM[v / 100]).append(" ").append(EN_UNIT[0]).append(" ");
+				v = v % 100; // 获取百位，并得到百位以后的数
+				if (v != 0) {
+					buf.append("and ");
+				} // 如果百位后的数不为0，则追加and
+			}
+			if (v != 0) { // 前提是v不为0才作解析
+				if (v < 20 || v % 10 == 0) {
+					// 如果小于20或10的整数倍，直接取基本数词表的单词
+					buf.append(EN_NUM[v]).append(" ");
+				} else { // 否则取10位数词，再取个位数词
+					buf.append(EN_NUM[v - v % 10]).append(" ");
+					buf.append(EN_NUM[v % 10]).append(" ");
+				}
+				if (i != count - 1) { // 百位以上的组追加相应的单位
+					buf.append(EN_UNIT[count - 1 - i]).append(" ");
+				}
+			}
+		}
+		return buf.toString().trim(); // 返回值
+	}
+
+}
